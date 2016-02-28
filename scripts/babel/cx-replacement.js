2ed1d4f4d048ff80b1172045e80263cf903893f0
Iegor Azuaga <dextrackmedia@gmail.com>
2016-02-28 20:08:21 +0100

    Set selectorMap default value to empty obj

--- scripts/babel/cx-replacement.js
+++ scripts/babel/cx-replacement.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-var selectorMap = null;
+var selectorMap = {};
 
 function replaceSlashes(str) {
   var selector = selectorMap[str] || str;
